630a1a59d78d401787b02842921ed246954cab84
introvertCode <stanislaw.bielski.programista@gmail.com>
2022-07-21 20:32:50 +0200

    new project started

---
+++ src/com/barosanu/controller/LoginWindowController.java
@@ -0,0 +1,31 @@
+package com.barosanu.controller;
+import com.barosanu.EmailManager;
+import com.barosanu.view.ViewFactory;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController extends BaseController {
+
+
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private TextField passwordField;
+
+    public LoginWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
+        super(emailManager, viewFactory, fxmlName);
+    }
+
+    @FXML
+    void loginButtonAction() {
+        System.out.println("clicked");
+
+    }
+
+
+}
